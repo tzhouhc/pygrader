af79d2dc9d1024c45120b992dd104d209ccb295f
Ting Zhou <zhoutingneyc@gmail.com>
2025-08-27 22:05:42 -0400

    fix: typing in hw_base.py

--- common/hw_base.py
+++ common/hw_base.py
@@ -1,31 +1,34 @@
 """hw.py: Base class for all HW's"""
 
+import json
 import os
 import sys
-from typing import Callable, Tuple, List
-import json
-from pathlib import Path
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Callable
 
-import common.utils as u
 import common.printing as printing
 import common.submissions as subs
+import common.utils as u
+
 
 @dataclass
-class RubricItem():
+class RubricItem:
     """Representation of a rubric item.
 
     Attributes:
         code: The code of this item (e.g. B1)
-        subitems: List containing (pts, desc) for each subitem (e.g. B1.1, B1.2)
+        subitems: list containing (pts, desc) for each subitem (e.g. B1.1, B1.2)
         tester: Callback function to grade this item.
     """
-    code: str
-    deduct_from: int
-    subitems: List[Tuple[int, str]]
-    tester: Callable
 
-class HW():
+    code: str
+    deduct_from: int | None
+    subitems: list[tuple[int, str]]
+    tester: Callable[..., Any]
+
+
+class HW:
     """Grading Base Class
 
     Here's a visual representation of some of the fields:
@@ -48,32 +51,31 @@ class HW():
             assignments, there is 1 submission_dir per student.
     """
 
-    def __init__(self, hw_name, rubric_name):
-        self.hw_name = hw_name
-        self.hw_workspace = os.path.join(Path.home(),
-                                         ".grade",
-                                         os.getenv("TA", default=''),
-                                         hw_name)
+    def __init__(self, hw_name: str, rubric_name: str):
+        self.hw_name: str = hw_name
+        self.hw_workspace: str = os.path.join(
+            Path.home(), ".grade", os.getenv("TA", default=""), hw_name
+        )
 
         # Find grader root relative to hw_base.py: root/common/hw_base.py
         pygrader_root = Path(__file__).resolve().parent.parent
 
-        self.scripts_dir = os.path.join(pygrader_root, self.hw_name)
+        self.scripts_dir: str = os.path.join(pygrader_root, self.hw_name)
 
         # Here we assume the rubric file is in the scripts dir.
         rubric_path = os.path.join(self.scripts_dir, rubric_name)
-        self.rubric = self.create_rubric(rubric_path)
+        self.rubric: dict[str, Any] = self.create_rubric(rubric_path)
 
-        self.submission_dir = None  # Populated in subclasses.
+        self.submission_dir: str | None = None  # Populated in subclasses.
 
-    def create_rubric(self, rubric_file):
+    def create_rubric(self, rubric_file: str) -> dict[str, Any]:
         """Parses a JSON rubric file into a Python representation."""
 
-        #TODO check if file exists
+        # TODO check if file exists
         with open(rubric_file, "r") as f:
-            rubric_json = json.load(f)
+            rubric_json: dict[str, Any] = json.load(f)
 
-        rubric = {}
+        rubric: dict[str, Any] = {}
         for table_k, table_v in rubric_json.items():
             if table_k == "late_penalty":
                 rubric[table_k] = table_v
@@ -82,19 +84,24 @@ class HW():
                 rubric[table_k] = {}
 
             for item in table_v:
-                deduct_from = None
-                if 'deducting_from' in table_v[item]:
-                    deduct_from = table_v[item]['deducting_from']
+                deduct_from: int | None = None
+                if "deducting_from" in table_v[item]:
+                    deduct_from = table_v[item]["deducting_from"]
                 ri_obg = RubricItem(
-                            table_v[item]['name'],
-                            deduct_from,
-                            list(zip(table_v[item]['points_per_subitem'],
-                                    table_v[item]['desc_per_subitem'])),
-                            getattr(self, "grade_" + item, self.default_grader))
+                    table_v[item]["name"],
+                    deduct_from,
+                    list(
+                        zip(
+                            table_v[item]["points_per_subitem"],
+                            table_v[item]["desc_per_subitem"],
+                        )
+                    ),
+                    getattr(self, "grade_" + item, self.default_grader),
+                )
                 rubric[table_k][item] = ri_obg
         return rubric
 
-    def do_cd(self, path):
+    def do_cd(self, path: str) -> None:
         """Changes directory relative to the self.submission_dir.
 
         For example, if you had the following:
@@ -105,11 +112,12 @@ class HW():
         and you wanted to cd into part1-sub, you would run
         `do_cd(os.path.join('part1', 'part1-sub'))`.
         """
-        part_dir = os.path.join(self.submission_dir, path)
+        assert self.submission_dir
+        part_dir: str = os.path.join(self.submission_dir, path)
         u.is_dir(part_dir)
         os.chdir(part_dir)
 
-    def exit_handler(self, _signal, _frame):
+    def exit_handler(self, _signal: Any, _frame: Any) -> None:
         """Handler for SIGINT
 
         Note: this serves as a template for how the subclasses should do it.
@@ -125,8 +133,10 @@ class HW():
         proc = u.cmd_popen("git log -n 1 --format='%aI'")
         iso_timestamp, _ = proc.communicate()
 
-        return subs.check_late(os.path.join(self.hw_workspace, "deadline.txt"),
-                               iso_timestamp.strip('\n'))
+        return subs.check_late(
+            os.path.join(self.hw_workspace, "deadline.txt"),
+            iso_timestamp.strip("\n"),
+        )
 
     def default_grader(self):
         """Generic grade function."""
@@ -139,7 +149,8 @@ class HW():
     def cleanup(self):
         """Performs cleanup (kills stray processes, removes mods, etc.)."""
 
-def directory(start_dir: str) -> Callable:
+
+def directory(start_dir: str) -> Callable[..., Any]:
     """Decorator function that cd's into `start_dir` before the test.
 
     If start_dir is 'root', we cd into the root of the submission_dir.
@@ -154,13 +165,16 @@ def directory(start_dir: str) -> Callable:
     # for the sake of making the decorator usage sleek), let's allow
     # users to just use '/'. We can correct it here.
     start_dir = os.path.join(*start_dir.split("/"))
-    def function_wrapper(test_func):
-        def cd_then_test(hw_instance):
+
+    def function_wrapper(test_func: Callable[..., Any]) -> Callable[..., Any]:
+        def cd_then_test(hw_instance: HW) -> Any:
             try:
-                hw_instance.do_cd('' if start_dir == "root" else start_dir)
+                hw_instance.do_cd("" if start_dir == "root" else start_dir)
             except ValueError:
-                printing.print_red("[ Couldn't cd into tester's @directory, "
-                                   "opening shell.. ]")
+                printing.print_red(
+                    "[ Couldn't cd into tester's @directory, "
+                    "opening shell.. ]"
+                )
                 os.system("bash")
             return test_func(hw_instance)
 
