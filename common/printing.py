14962ad152908b4945a2ca7655b241ef809a1ae6
Ting Zhou <zhoutingneyc@gmail.com>
2025-08-28 13:46:35 -0400

    fix: typing in printing.py

--- common/printing.py
+++ common/printing.py
@@ -1,96 +1,112 @@
 """printing.py: Colored output helpers"""
 
-CEND = '\33[0m'
-CBOLD = '\33[1m'
-CITALIC = '\33[3m'
-CURL = '\33[4m'
-CBLINK = '\33[5m'
-CBLINK2 = '\33[6m'
-CSELECTED = '\33[7m'
+from typing import Any
 
-CBLACK = '\33[30m'
-CRED = '\33[31m'
-CGREEN = '\33[32m'
-CYELLOW = '\33[33m'
-CBLUE = '\33[34m'
-CVIOLET = '\33[35m'
-CBEIGE = '\33[36m'
-CWHITE = '\33[37m'
+CEND = "\33[0m"
+CBOLD = "\33[1m"
+CITALIC = "\33[3m"
+CURL = "\33[4m"
+CBLINK = "\33[5m"
+CBLINK2 = "\33[6m"
+CSELECTED = "\33[7m"
 
-CBLACKBG = '\33[40m'
-CREDBG = '\33[41m'
-CGREENBG = '\33[42m'
-CYELLOWBG = '\33[43m'
-CBLUEBG = '\33[44m'
-CVIOLETBG = '\33[45m'
-CBEIGEBG = '\33[46m'
-CWHITEBG = '\33[47m'
+CBLACK = "\33[30m"
+CRED = "\33[31m"
+CGREEN = "\33[32m"
+CYELLOW = "\33[33m"
+CBLUE = "\33[34m"
+CVIOLET = "\33[35m"
+CBEIGE = "\33[36m"
+CWHITE = "\33[37m"
 
-CGREY = '\33[90m'
-CRED2 = '\33[91m'
-CGREEN2 = '\33[92m'
-CYELLOW2 = '\33[93m'
-CBLUE2 = '\33[94m'
-CVIOLET2 = '\33[95m'
-CCYAN = '\33[96m'
-CGRAYL = '\33[97m'
+CBLACKBG = "\33[40m"
+CREDBG = "\33[41m"
+CGREENBG = "\33[42m"
+CYELLOWBG = "\33[43m"
+CBLUEBG = "\33[44m"
+CVIOLETBG = "\33[45m"
+CBEIGEBG = "\33[46m"
+CWHITEBG = "\33[47m"
 
-CGREYBG = '\33[100m'
-CREDBG2 = '\33[101m'
-CGREENBG2 = '\33[102m'
-CYELLOWBG2 = '\33[103m'
-CBLUEBG2 = '\33[104m'
-CVIOLETBG2 = '\33[105m'
-CBEIGEBG2 = '\33[106m'
-CWHITEBG2 = '\33[107m'
+CGREY = "\33[90m"
+CRED2 = "\33[91m"
+CGREEN2 = "\33[92m"
+CYELLOW2 = "\33[93m"
+CBLUE2 = "\33[94m"
+CVIOLET2 = "\33[95m"
+CCYAN = "\33[96m"
+CGRAYL = "\33[97m"
 
-def print_red(s):
+CGREYBG = "\33[100m"
+CREDBG2 = "\33[101m"
+CGREENBG2 = "\33[102m"
+CYELLOWBG2 = "\33[103m"
+CBLUEBG2 = "\33[104m"
+CVIOLETBG2 = "\33[105m"
+CBEIGEBG2 = "\33[106m"
+CWHITEBG2 = "\33[107m"
+
+
+def print_red(s: str) -> None:
     """Prints s in red"""
     print("{}{}{}".format(CRED2, s, CEND))
 
-def print_green(s):
+
+def print_green(s: str) -> None:
     """Prints s in green"""
     print("{}{}{}".format(CGREEN2, s, CEND))
 
-def print_yellow(s):
+
+def print_yellow(s: str) -> None:
     """Prints s in yellow"""
     print("{}{}{}".format(CYELLOW2, s, CEND))
 
-def print_magenta(s):
+
+def print_magenta(s: str) -> None:
     """Prints s in magenta"""
     print("{}{}{}".format(CVIOLET2, s, CEND))
 
-def print_purple(s):
+
+def print_purple(s: str) -> None:
     """Prints s in purple"""
     print("{}{}{}".format(CVIOLET, s, CEND))
 
-def print_cyan(s):
-    """Prints s in cyan"""
-    print("{}{}{}" .format(CCYAN, s, CEND))
 
-def print_light_gray(s):
+def print_cyan(s: str) -> None:
+    """Prints s in cyan"""
+    print("{}{}{}".format(CCYAN, s, CEND))
+
+
+def print_light_gray(s: str) -> None:
     """Prints s in light gray"""
     print("{}{}{}".format(CGRAYL, s, CEND))
 
-def print_line():
-    print_cyan('-'*85)
 
-def print_double():
-    print_cyan('='*85)
+def print_line() -> None:
+    print_cyan("-" * 85)
 
-def print_intro(team, hw, code):
+
+def print_double() -> None:
+    print_cyan("=" * 85)
+
+
+def print_intro(team: str, hw: str, code: str) -> None:
     print_double()
-    print(f"{CCYAN}Name:{CEND} {CVIOLET2}{team}{CEND}  {CCYAN}HW:{CEND} "
-          f"{CVIOLET2}{hw}{CEND}  {CCYAN}Rubric Code:{CEND} "
-          f"{CVIOLET2}{code}{CEND}")
+    print(
+        f"{CCYAN}Name:{CEND} {CVIOLET2}{team}{CEND}  {CCYAN}HW:{CEND} "
+        f"{CVIOLET2}{hw}{CEND}  {CCYAN}Rubric Code:{CEND} "
+        f"{CVIOLET2}{code}{CEND}"
+    )
     print_double()
 
-def print_between_cyan_line(msg):
+
+def print_between_cyan_line(msg: str) -> None:
     print_line()
     print_cyan(msg)
     print_line()
 
-def print_outro(table_item):
+
+def print_outro(table_item: Any) -> None:
     print_line()
     print_green("End test of {}".format(table_item))
     print_double()
