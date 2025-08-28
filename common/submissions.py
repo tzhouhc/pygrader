84bf18c969dd773a5f2fae30301df86d53ba7759
Ting Zhou <zhoutingneyc@gmail.com>
2025-08-28 14:08:54 -0400

    fix: format/typing in submissions.py

--- common/submissions.py
+++ common/submissions.py
@@ -1,14 +1,17 @@
 """submissions.py: Utils that help dealing with submissions"""
 
 import os
-from typing import Callable
 from datetime import datetime
+from typing import Any, Callable
+
+import git
 from dateutil.relativedelta import relativedelta
 from pytz import timezone
-import git
+
 import common.printing as printing
 
-def check_late(deadline_path, iso_timestamp):
+
+def check_late(deadline_path: str, iso_timestamp: str) -> bool:
     """Checks if iso_timestamp is past the deadline
 
     Arguments:
@@ -35,15 +38,18 @@ def check_late(deadline_path, iso_timestamp):
         return False
 
     diff = relativedelta(submission, deadline)
-    printing.print_red(f"[SUBMISSION LATE]: Submitted {diff.days} days, "
-                       f"{diff.hours} hrs, {diff.minutes} mins, "
-                       f"and {diff.seconds} secs late")
+    printing.print_red(
+        f"[SUBMISSION LATE]: Submitted {diff.days} days, "
+        f"{diff.hours} hrs, {diff.minutes} mins, "
+        f"and {diff.seconds} secs late"
+    )
 
     return True
 
+
 def checkout_to_team_branch(
-        repo: git.Repo, team_repo_id: str,
-        team: str, branch_name: str = "master") -> bool:
+    repo: git.Repo, team_repo_id: str, team: str, branch_name: str = "master"
+) -> bool:
     repo.git.checkout(branch_name)  # Make sure we're on the branch (skel).
     try:
         repo.git.remote("rm", team)
@@ -66,7 +72,7 @@ def checkout_to_team_branch(
     # Let's checkout to the team's branch.
     team_branch = f"{team}-{branch_name}"
     try:
-        repo.git.branch('-D', team_branch)  # Just in case it exists already.
+        repo.git.branch("-D", team_branch)  # Just in case it exists already.
     except git.GitError:
         # This branch doesn't exist.
         pass
@@ -77,11 +83,12 @@ def checkout_to_team_branch(
         except git.GitError:
             repo.git.checkout("-b", team_branch, f"{team}/main")
     else:
-            repo.git.checkout("-b", team_branch, f"{team}/{branch_name}")
+        repo.git.checkout("-b", team_branch, f"{team}/{branch_name}")
 
     return True
 
-def tag(tag_name: str) -> Callable:  # pylint: disable=unused-argument
+
+def tag(tag_name: str) -> Callable[..., Any]:  # pylint: disable=unused-argument
     """Decorator function that checks out to tag_name before the test.
 
     If tag_name is 'master', we checkout to the submission's master branch,
@@ -102,14 +109,16 @@ def tag(tag_name: str) -> Callable:  # pylint: disable=unused-argument
                         tag_name = f"{hw_instance.submitter}"
                         hw_instance.repo.git.checkout(tag_name)
                         printing.print_green(
-                                f"[ Checked out to {tag_name}/master ]\n")
+                            f"[ Checked out to {tag_name}/master ]\n"
+                        )
                     else:
                         hw_instance.repo.git.checkout(tag_name)
                         printing.print_green(f"[ Checked out to {tag_name} ]\n")
                 except git.GitError:
                     printing.print_red(f"[ Couldn't checkout to {tag_name} ]")
                     printing.print_cyan(
-                            "[ Opening shell -- ^D/exit when resolved ]")
+                        "[ Opening shell -- ^D/exit when resolved ]"
+                    )
                     os.system("bash")
             else:
                 # No cleaning in case the TA made necessary changes to the
@@ -123,6 +132,7 @@ def tag(tag_name: str) -> Callable:  # pylint: disable=unused-argument
 
     return function_wrapper
 
+
 def to_branch(hw_instance, branch_name: str):
     current_branch = hw_instance.repo.git.rev_parse("--abbrev-ref", "HEAD")
     target_branch = f"{hw_instance.submitter}-{branch_name}"
@@ -134,8 +144,7 @@ def to_branch(hw_instance, branch_name: str):
             printing.print_green(f"[ Checked out to {branch_name} ]\n")
         except git.GitError:
             printing.print_red(f"[ Couldn't checkout to {branch_name} ]")
-            printing.print_cyan(
-                    "[ Opening shell -- ^D/exit when resolved ]")
+            printing.print_cyan("[ Opening shell -- ^D/exit when resolved ]")
             os.system("bash")
     else:
         # No cleaning in case the TA made necessary changes to the
@@ -144,7 +153,10 @@ def to_branch(hw_instance, branch_name: str):
 
     hw_instance.repo.git.clean("-f", "-d")
 
-def branch(branch_name: str) -> Callable:  # pylint: disable=unused-argument
+
+def branch(
+    branch_name: str,
+) -> Callable[..., Any]:  # pylint: disable=unused-argument
     """Decorator function that checks out submitter-branch_name before the test.
 
     This is assumed to be in reference to a submission branch, which follows the
@@ -152,8 +164,8 @@ def branch(branch_name: str) -> Callable:  # pylint: disable=unused-argument
     If the checkout fails, we open a shell for the grader to resolve the issue.
     """
 
-    def function_wrapper(test_func):
-        def checkout_to_branch_then_test(hw_instance):
+    def function_wrapper(test_func: Callable[..., Any]) -> Callable[..., Any]:
+        def checkout_to_branch_then_test(hw_instance) -> Any:
             nonlocal branch_name
             to_branch(hw_instance, branch_name)
             return test_func(hw_instance)
@@ -161,6 +173,7 @@ def branch(branch_name: str) -> Callable:  # pylint: disable=unused-argument
         return checkout_to_branch_then_test
 
     return function_wrapper
+
 
 def apply_patch(repo: git.Repo, patch_path: str) -> bool:
     """Applies a patch to the current dir (assumed to be a repo)"""
